c640ae38baa5951e56a8c30a3f68176a0730af94
Glen Dobson <g.dobson@lancaster.ac.uk>
2026-01-27 13:12:04 +0000

    Minor rails update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_11_27_154316) do
+ActiveRecord::Schema[8.1].define(version: 2026_01_27_130622) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
